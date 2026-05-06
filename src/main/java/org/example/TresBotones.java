9ed353435a0aa7a71407bda437cdd60a2fdf4ee9
Dibo72 <humanida133@gmail.com>
2026-05-06 12:40:13 +0200

    Ejercicio acabado

--- src/main/java/org/example/TresBotones.java
+++ src/main/java/org/example/TresBotones.java
@@ -1,17 +1,24 @@
 package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+public class TresBotones extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Button button1 = new Button("Botón 1");
+        Button button2 = new Button("Botón 2");
+        Button button3 = new Button("Botón 3");
+        HBox hbox = new HBox(button1, button2);
+        VBox vbox = new VBox(hbox, button3);
+        Scene scene = new Scene(vbox, 300, 200);
+        primaryStage.setTitle("Ejemplo de Layouts en JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        launch(args);
     }
 }
